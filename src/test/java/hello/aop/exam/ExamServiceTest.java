bd20b74c5db3a774a42033d20544b83c3e6f8d72
Kidding Just <rlarkdlr@gmail.com>
2021-11-28 17:55:22 +0900

    aspectJ, aop

---
+++ src/test/java/hello/aop/exam/ExamServiceTest.java
@@ -0,0 +1,25 @@
+package hello.aop.exam;
+
+import hello.aop.exam.aop.TraceAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+@Import(TraceAspect.class)
+@Slf4j
+class ExamServiceTest {
+
+    @Autowired
+    ExamService examService;
+
+    @Test
+    void test(){
+        for(int i=1; i<6; i++){
+            log.info("client request i={}", i);
+            examService.request("data" + i);
+        }
+    }
+}
